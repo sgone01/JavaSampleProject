67d0f2e259005d53dfbd1a52284104cb81cfcd4c
Sudarashan Gonewar <112791583+sudarshangonewar@users.noreply.github.com>
2023-12-30 15:48:49 +0530

    Update Main.java
    Add new function

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 import java.util.Date;
 
 public class Main{
+    public void callingFunc(){
+        System.out.println("From Calling Functions");
+    }
     public static void main(String[] args){
         System.out.println("Hello!!!");
         System.out.println("This is from JavaSampleProject");
         System.out.println(new Date());
+        new Main().callingFunc();
     }
 }
