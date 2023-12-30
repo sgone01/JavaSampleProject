8dcc86ee870eb7cff3779e2f543e5949041757c9
Sudarashan Gonewar <112791583+sudarshangonewar@users.noreply.github.com>
2023-12-30 15:38:02 +0530

    Create Main.java
    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello!!!");
+        System.out.println("This is from JavaSampleProject");
+        System.out.println(new Date());
+    }
+}
